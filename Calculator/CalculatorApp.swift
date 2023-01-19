aa860d1c0c4a5f02ce58f1e29284f6664a62ed88
daulet <dauletkd01@gmail.com>
2023-01-19 16:05:03 +0600

    ios_calc

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Daulet on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
